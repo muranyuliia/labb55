74d37dd59cbb7cff27d0ef22699ac13164249ef1
muranyuliia <146208756+muranyuliia@users.noreply.github.com>
2023-10-31 21:44:15 +0200

    two

--- labb55/labb55.cpp
+++ labb55/labb55.cpp
@@ -1,33 +1,34 @@
 #include <iostream>
 using namespace std;
+
 // Рекурсивна функція для представлення числа в системі числення з основою base.
 void decimalToBase(int n, int base) {
-    if (n > base - 1) {
+    if (n > 0) {
         decimalToBase(n / base, base);
+        cout << n % base;
     }
-    cout << n % base;
 }
 
 int main() {
     int decimalNumber, choice;
 
-    cout << "natural number: ";
+    cout << "number: ";
     cin >> decimalNumber;
 
     if (decimalNumber < 0) {
-        cerr << "no natural." << endl;
+        cerr << "nо." << endl;
         return 1;
     }
 
-    cout << "2 or 8 ";
+    cout << "2 or 8: ";
     cin >> choice;
 
     if (choice != 2 && choice != 8) {
-        cerr << "nepidtrymuvana systema chyslenya" << endl;
+        cerr << "Непідтримувана система числення." << endl;
         return 1;
     }
 
-    cout << "predstavlenya number: ";
+    cout << "result: ";
     decimalToBase(decimalNumber, choice);
     cout << endl;
 
